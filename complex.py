b5198c32b99c0ce6cb1de651944d2d746d59bf75
SujanMaga <sujan.magar98765@gmail.com>
2020-12-09 03:44:36 +0545

    Complex-Number

--- complex.py
+++ complex.py
@@ -1,1 +1,25 @@
-# (a+bi)(c+di) = (ac-bd) + (ad+cb)i
+# (a+bi)(c+di) = (ac-bd) + (bc+ad)i
+# (a+bi)+(c+di) = (a+c) + (b+d)i
+
+# deducing the formaula into a program.
+class Complex:
+    def __init__(self, r , i):
+        self.real = r
+        self.imaginary = i
+
+    def __add__(self, other):
+        return f"{self.real + other.real} + {self.imaginary + other.imaginary}i"
+
+    def __mul__(self, other):
+        real = (self.real * other.real) - (self.imaginary * other.imaginary)
+        imaginary = (self.imaginary * other.real) + (self.real * other.imaginary)
+        return f"{real} + {imaginary}i" 
+
+    def __str__(self):
+        return f"{self.real} + {self.imaginary}i"
+
+# taking example
+c1 = Complex(2, 5)
+c2 = Complex(4, -3)
+print (c2 * c1 )
+print (c2 + c1 )
